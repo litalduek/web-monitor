168b9c13167cc0383438adc7393e2d0cd3003802
litalfine <litalduek@gmail.com>
2024-06-13 14:42:29 +0300

    commit

--- utils/Logger.py
+++ utils/Logger.py
@@ -1,17 +1,7 @@
 import logging
 import logging.config
 
-class SingletonType(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(SingletonType, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
 class Logger:
-    _instance = None
-
     def __init__(self):
         logging.basicConfig(
             level=logging.INFO,
@@ -24,5 +14,3 @@ class Logger:
 
     def get_logger(self):
         return self.logger
-
-    # attribution: https://gist.github.com/Rustam-Z/aeb88b01b0f0d84e9f23240eeea2727f
